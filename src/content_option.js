5c2ff080aa6f05f0c8a0f36a2ba9a3316bf1350d
Kevin <kevinhamelink05@gmail.com>
2025-04-15 11:15:46 +0200

    Mr handsome killed himself <3

---
+++ src/content_option.js
@@ -0,0 +1,78 @@
+const logotext = "Emiel";
+const meta = {
+    title: "Emiel",
+    description: "Portfolio",
+};
+
+const introdata = {
+    title: "Hi I'm Emiel",
+    animated: {
+        first: "I love miauw",
+        second: "I'm a cat"
+    },
+    description: "I'm a cat",
+    your_img_url: "",
+};
+
+const dataabout = {
+    title: "About me",
+    aboutme: "I'm a cat",
+};
+
+const worktimeline = [
+    {
+        jobtitle: "Cat",
+        where: "Cat",
+        date: "2022",
+    },
+];
+
+const skills = [
+    {
+        name: "C#",
+        value: 90,
+    },
+];
+
+const services = [
+    {
+        title: "Cat",
+        description: "I'm a cat",
+    },
+];
+
+const dataportfolio = [{
+    img: "https://i.pinimg.com/474x/3a/63/54/3a63544cab1e6926dcb7f02c384be94d.jpg",
+    description: "A game slice of Hollow Knight, developed using Unity, C# and my team.",
+    link: "https://github.com/Interis-mk/Game-Sclice",
+},
+    {
+        img: "https://github.com/Interis-mk/delphinitysd-pokemon-students-1ae0965c0c32-main/blob/main/image.png?raw=true",
+        description: "This game has been made during my study at Software developer @ Scalda It's a 'simple' console pokemon game made in java.",
+        link: "https://github.com/Interis-mk/delphinitysd-pokemon-students-1ae0965c0c32-main",
+    },
+    {
+        img: "https://play-lh.googleusercontent.com/O2lZnoj9FjC650wQDsvnlsYtZx8wfMBb-RqoMCJJEwcoowON0hquaJFgMe_3etra1MU",
+        description: "A tower defence game, developed using Unity, C# and myself. This games art is based on Kingdom rush.",
+        link: "https://github.com/Interis-mk/TowerDefence",
+    },
+];
+
+const socialprofils = {
+    github: "https://github.com/Interis-mk",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/kevin-hamelink/",
+    discord: "https://discordapp.com/users/394508922091995137",
+};
+
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    socialprofils,
+    logotext,
+};
